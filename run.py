801e0fb245c03dece366de54b0c6b4d6f864189d
kaminuma <>
2025-07-06 04:43:50 +0900

    修正

--- run.py
+++ run.py
@@ -3,10 +3,15 @@
 from app import create_app
 import logging
 from waitress import serve
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
     # ロギングの設定を詳細に
     logging.basicConfig(level=logging.DEBUG)
-    serve(app, host='0.0.0.0', port=5001)
+    
+    # 環境変数からポートを取得（デフォルトは5000）
+    port = int(os.getenv('PORT', 5000))
+    
+    serve(app, host='0.0.0.0', port=port)
